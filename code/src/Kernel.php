7b27424f8f1db8b064f5143043881e59f558ab04
dykyi.roman <mr.dukuy@gmail.com>
2024-11-22 12:41:42 +0400

    add services config

--- code/src/Kernel.php
+++ code/src/Kernel.php
@@ -7,6 +7,9 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Finder\Finder;
 
 class Kernel extends BaseKernel
@@ -18,6 +21,11 @@ class Kernel extends BaseKernel
         $configDir = $this->getConfigDir();
         $routes->import($configDir.'/routes.yaml');
 
+        $this->loadDomainConfigurations($routes);
+    }
+
+    protected function loadDomainConfigurations(RoutingConfigurator $routes): void
+    {
         $projectDir = $this->getProjectDir();
         $finder = new Finder();
         $finder->directories()
@@ -49,5 +57,31 @@ class Kernel extends BaseKernel
                 $routes->import($routesPath);
             }
         }
+    }
+
+    protected function buildContainer(): ContainerBuilder
+    {
+        $container = parent::buildContainer();
+        
+        // Load domain services
+        $projectDir = $this->getProjectDir();
+        $finder = new Finder();
+        $finder->directories()
+            ->in($projectDir.'/src')
+            ->depth(0)
+            ->notName('Kernel.php');
+
+        // Load services
+        $loader = new YamlFileLoader($container, new FileLocator());
+        foreach ($finder as $domainDir) {
+            $domainPath = $domainDir->getRealPath();
+            $servicesPath = $domainPath.'/Resources/Config/services.yaml';
+
+            if (file_exists($servicesPath)) {
+                $loader->load($servicesPath);
+            }
+        }
+
+        return $container;
     }
 }
