0bda59d3022bc077de1a56e50c21cd027780ff52
dykyi.roman <mr.dukuy@gmail.com>
2025-06-02 12:49:03 +0400

    refactring

---
+++ code/src/Shared/Application/Service/ApplicationService.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Application\Service;
+
+use Psr\Log\LoggerInterface;
+use Shared\Application\Exception\ApplicationException;
+use Shared\DomainModel\Enum\GeneralErrorCode;
+use Shared\DomainModel\Exception\DomainException;
+use Shared\DomainModel\Service\MessageBusInterface;
+use Throwable;
+
+final readonly class ApplicationService
+{
+    public function __construct(
+        private MessageBusInterface $commandBus,
+        private MessageBusInterface $queryBus,
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    public function command(object $command): void
+    {
+        try {
+            $this->commandBus->dispatch($command);
+        } catch (DomainException $exception) {
+            $this->logger->error($exception->getMessage(), $exception->jsonSerialize());
+        } catch (Throwable $exception) {
+            $this->logger->error($exception->getMessage());
+        }
+    }
+
+    /**
+     * @throws \Shared\Application\Exception\ApplicationException
+     */
+    public function query(object $query): mixed
+    {
+        try {
+            return $this->queryBus->dispatch($query);
+        } catch (DomainException $exception) {
+            throw new ApplicationException(
+                get_class($query),
+                $exception->getErrorCode(),
+                'Query execution failed',
+                $exception->context,
+                $exception,
+            );
+        } catch (Throwable $exception) {
+            throw new ApplicationException(
+                get_class($query),
+                GeneralErrorCode::UNEXPECTED_ERROR,
+                'Query execution failed',
+                [],
+                $exception,
+            );
+        }
+    }
+}
---
+++ code/src/Shared/DomainModel/Enum/GeneralErrorCode.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\DomainModel\Enum;
+
+enum GeneralErrorCode: int
+{
+    case UNEXPECTED_ERROR = 0;
+}
